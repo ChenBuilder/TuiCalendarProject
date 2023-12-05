fea044fe16302a42ed105c215c928a3e0d645018
TUI Chen <rchin@gws.dev.pc-daiwabo.co.jp>
2023-12-05 11:46:25 +0900

    update for nginx configration

--- src/frontend/webpack.config.cjs
+++ src/frontend/webpack.config.cjs
@@ -1,6 +1,7 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
   entry: './src/index.js',
@@ -28,7 +29,14 @@ module.exports = {
       template: './public/index.html',
       filename: 'index.html',
     }),
-    new MiniCssExtractPlugin(),
+    new MiniCssExtractPlugin({
+      filename: 'styles/[name].css',
+    }),  
+    new CopyWebpackPlugin({
+      patterns: [
+        { from: 'public/styles', to: 'styles' },
+      ],
+    }), 
   ],
   devServer: {
     static: {
